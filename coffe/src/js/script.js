d9edc15210e433df6e50d18153a0f64fad0d65b0
IvanStarov <77104225+IvanStarov@users.noreply.github.com>
2021-02-01 13:57:43 +0300

    Add files via upload
    Первый длинный сайт

---
+++ coffe/src/js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+      $('.burger').click(function(event){
+      	$('.burger, .navigation').toggleClass('active');
+      	$('body').toggleClass('lock');
+      });
+  });
+
+$(document).ready(function(){
+  $('.branches').slick({
+  	slidesToShow: 3,
+    dots: false,
+    arrows:false,
+    autoplay:false,
+    autoplaySpeed:4000,
+    responsive: [
+    {
+      breakpoint: 1301,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+    ]
+  });
+});
